10f761311190cc8c333dca896f2c3609ff47f542
ZhuYongle <iamzhulele@163.com>
2024-03-25 11:36:31 +0800

    成功集成avcodec

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,25 @@
 #include <jni.h>
+
 #include <string>
+#include <android/log.h>
+
+#define LOG_TAG  "C_TAG"
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO,LOG_TAG ,__VA_ARGS__) // 定义LOGI类型
+
+extern "C" {
+#include <libavcodec/avcodec.h>
+}
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_nativeapplication_MainActivity_stringFromJNI(
-        JNIEnv* env,
+        JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+    /*  std::string hello = "Hello from C++";
+      return env->NewStringUTF(hello.c_str());*/
+    //std::string hello = "avcodec configuration info ";
+    std::string config = avcodec_configuration();
+
+    LOGI("configuration %s\n", config.c_str());
+
+    return env->NewStringUTF(config.c_str());
 }
